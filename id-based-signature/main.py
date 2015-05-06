38112df52ae629eb24f1dd81a6bb7d7165b5d073
Romain SOMMERARD <rsommerard@gmail.com>
2015-05-06 16:24:24 +0200

    Update

--- id-based-signature/main.py
+++ id-based-signature/main.py
@@ -2,6 +2,7 @@ import client
 import random
 import time
 import hashlib
+import base64
 
 BASE_URL = 'http://pac.bouillaguet.info/TP5/id-based-signature'
 
@@ -17,11 +18,38 @@ if __name__ == '__main__':
     n = response['n']
     e = response['e']
 
+    i = ''
+    for j in range(16):
+        tmp = "{0:08x}".format(j)
+        if len(tmp) % 2 != 0:
+            tmp = '0' + tmp
+        tmp = tmp.upper()
+        print(tmp)
+
+        sha = hashlib.sha256()
+        sha.update('sommerard'.encode())
+        sha.update(base64.b16decode(tmp))
+        tmp = sha.hexdigest()
+
+        print('i:', i)
+        print('tmp:', tmp)
+        i += tmp
+        print('>i:', i)
+
+    i = int(i, base=16) % n
     response = server.query('/KDC/keygen/sommerard')
 
     secret_key = response['secret-key']
 
-    i = pow(secret_key, e, n)
+    print(pow(i, e, n))
+    print(secret_key)
+    print(i)
+    print(pow(secret_key, e, n))
+    if pow(i, e, n) == secret_key:
+        print('i ok!')
+    else:
+        print('i error!')
+        exit(1)
 
     r = random.randint(1, pow(2, 128))
 
@@ -29,9 +57,15 @@ if __name__ == '__main__':
 
     m = '#YOLO'
 
+    tmp = "{0:08x}".format(t)
+    if len(tmp) % 2 != 0:
+        tmp = '0' + tmp
+
+    tmp = tmp.upper()
+
     sha = hashlib.sha256()
     sha.update(m.encode())
-    sha.update("{0:08x}".format(t).encode())
+    sha.update(base64.b16decode(tmp))
     tmp = sha.hexdigest()
     print(tmp)
 
